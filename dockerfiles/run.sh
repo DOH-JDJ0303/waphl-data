297a1b73d4c20051ccc4a364da61abb6e779cdc8
DOH-JDJ0303 <jared.johnson@doh.wa.gov>
2024-11-15 11:56:00 -0800

    changing how info is passed via dockerfile

---
+++ dockerfiles/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#----- INPUTS -----#
+S3_BUCKET=$1
+S3_KEY=$2
+OUTDIR=$3
+
+#----- DETERMINE WORKFLOW & RUN DIRECTORY -----#
+# use file name to determine the workflow and how the run
+# name should be structured within the object key path
+FILENAME=$(basename "${S3_KEY}")
+case "${FILENAME}" in
+    "Phoenix_Summary.tsv")
+        WORKFLOW="phoenix"
+        RUNDIR=$(echo ${S3_KEY} | tr '/' '\n' | head -n -1 | tr '\n' '/')
+        ;;
+    *)
+        echo "Filename does not match an expected pattern" && exit 1
+        ;;
+esac
+
+#----- BUILD SAMPLESHEET -----#
+echo "workflow,run" > samplesheet.csv
+echo "${WORKFLOW},${RUNDIR}" >> samplesheet.csv
+echo -e "\nRunning samplesheet:" && cat samplesheet.csv && echo -e "\n"
+
+#----- RUN PIPELINE -----#
+# clone waphl-data repo
+rm -r waphl-data > /dev/null 2>&1
+git clone https://github.com/DOH-JDJ0303/waphl-data.git && ls waphl-data/
+# run waphl-prod2res
+CMD="nextflow run waphl-data/waphl-prod2res/main.nf --input samplesheet.csv --retention_schema waphl-prod2res/retention-schemes.config --outdir ${OUTDIR}"
+echo -e "\nCMD: ${CMD}\n"
+${CMD}
+
+#----- RUN PIPELINE -----#
+TIMESTAMP=$(cat .nextflow.log | grep "Files will be saved with timestamp:" | cut -f 4 -d ':' | tr -d ' ')
+aws s3 cp .nextflow.log ${OUTDIR%%/}/logs/${TIMESTAMP}-waphl-prod2res.log
