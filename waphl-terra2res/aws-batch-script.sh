686f71b0cae56e3833a31531ca93fb3442d84076
DOH-JDJ0303 <jared.johnson@doh.wa.gov>
2024-11-21 11:08:34 -0800

    finishing up

---
+++ waphl-terra2res/aws-batch-script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#----- INPUTS -----#
+TERRA_PROJECT=$1
+TERRA_WORKSPACE=$2
+TERRA_SUBMISSIONID=$3
+TERRA_WORKFLOW=$4
+TERRA_SUBMISSIONENTITY=$5
+OUTDIR=$6
+GCRED=$7
+
+#----- REPORT INPUTS -----#
+echo -e "\nInput Summary:"
+echo "TERRA_PROJECT: ${TERRA_PROJECT}"
+echo "TERRA_WORKSPACE: ${TERRA_WORKSPACE}"
+echo "TERRA_SUBMISSIONID: ${TERRA_SUBMISSIONID}"
+echo "TERRA_WORKFLOW: ${TERRA_WORKFLOW}"
+echo "TERRA_SUBMISSIONENTITY: ${TERRA_SUBMISSIONENTITY}"
+echo "S3_OUTDIR: ${S3_OUTDIR}"
+echo "GCRED: ${GCRED}"
+echo -e "\n"
+
+#----- CONFIGURE GCLOUD -----#
+aws s3 cp $GCRED google_credentials.json
+export GOOGLE_APPLICATION_CREDENTIALS='google_credentials.json'
+
+#----- BUILD SAMPLESHEET -----#
+echo -e "\nCreating Samplesheet:"
+echo "project,workspace,workflow,run" > samplesheet.csv
+echo "${TERRA_PROJECT},${TERRA_WORKSPACE},${TERRA_WORKFLOW},${TERRA_SUBMISSIONENTITY}" >> samplesheet.csv
+cat samplesheet.csv && echo -e "\n"
+
+#----- RUN PIPELINE -----#
+echo -e "\nStarting pipeline:"
+# run waphl-prod2res
+CMD="nextflow run waphl-data/waphl-prod2res/main.nf --input samplesheet.csv --outdir s3://${OUTDIR}
+echo -e "\nCMD: ${CMD}\n"
+${CMD}
+
+#----- UPDATE LOG FILE -----#
+echo -e "\nUpdating log file"
+TIMESTAMP=$(cat .nextflow.log | grep "Files will be saved with timestamp:" | cut -f 4 -d ':' | tr -d ' ')
+aws s3 cp .nextflow.log ${OUTDIR%%/}/logs/${TIMESTAMP}-waphl-terra2res.log
+
+#----- CACHE SUBMISSION ID -----#
+touch empty_file && aws s3 cp empty_file s3://${OUTDIR}/cache/terra/${TERRA_PROJECT}/${TERRA_WORKFLOW}/${TERRA_SUBMISSIONID}"
+
+echo -e "\nPipeline Complete!"
--- waphl-terra2res/terraRunChecker/lambda_function.py
+++ waphl-terra2res/terraRunChecker/lambda_function.py
@@ -1,6 +1,8 @@
-# determines new Terra runs using the submission ID and submits an SQS message
-# checks submission ID cache if it is available, otherwise all runs are submitted
-
+"""
+Determines new Terra runs using the submission ID and submits an AWS Batch job for each.
+Checks submission ID cache if it is available, otherwise all runs are submitted.
+Cache is updated at the end of batch job
+"""
 from firecloud import api as fapi
 import json
 from datetime import datetime
@@ -9,7 +11,7 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-def terraRunChecker(project,workspace,bucket,queue_url):
+def terraRunChecker(project,workspace,bucket,jobqueue,jobdef,gcred):
     # create dictionary of Terra entities (runs) and their submission IDs
     ## get list of Terra submissions
     r = fapi.list_submissions(project, workspace)
@@ -20,7 +22,7 @@ def terraRunChecker(project,workspace,bucket,queue_url):
         runst    = elem["status"] == "Done"
         samplest = "Succeeded" in elem["workflowStatuses"]
         if not setst and runst and samplest:
-            runs[elem["submissionId"]] = elem["submissionEntity"]["entityType"]
+            runs[elem["submissionId"]] = [ elem["methodConfigurationName"], elem["submissionEntity"]["entityType"] ]
 
     # determine if there any new runs based on the submission IDs and existing ID cache
     cache_key = f'cache/terra/{project}/{workspace}/'
@@ -35,22 +37,54 @@ def terraRunChecker(project,workspace,bucket,queue_url):
     else:
         newruns = runs
     
-    # send SQS message for each new run
-    # Create an SQS client 
-    sqs = boto3.client('sqs')
-    #URL of the SQS queue  
+    # submit a batch job for each new run
+    batch_client = boto3.client('batch')
+    # limit to 10 runs 
+    if len(newruns) > 10:
+        newruns = newruns[:9]
     for run in newruns:
-        # Message to send
-        msg =  f'{{"project":"{project}", "workspace":"{workspace}", "submissionId":"{run}", "submissionEntity":"{newruns[run]}"}}'
-        print(msg)
-        # Send the message
-        response = sqs.send_message( QueueUrl=queue_url, MessageBody=msg ) 
-        # Print out the response 
-        print(response)
+        response = batch_client.submit_job(
+                jobName=f'{project}_{workspace}_{run}',
+                jobQueue=jobqueue,
+                jobDefinition=jobdef,
+                containerOverrides={
+                    'environment': [
+                        {
+                            'name': 'TERRA_PROJECT',
+                            'value': project
+                        },
+                        {
+                            'name': 'TERRA_WORKSPACE',
+                            'value': workspace
+                        },
+                        {
+                            'name': 'TERRA_SUBMISSIONID',
+                            'value': run
+                        },
+                        {
+                            'name': 'TERRA_WORKFLOW',
+                            'value': newruns[run][0]
+                        },
+                        {
+                            'name': 'TERRA_SUBMISSIONENTITY',
+                            'value': newruns[run][1]
+                        },
+                        {
+                            'name': 'S3_OUTDIR',
+                            'value': bucket
+                        },
+                        {
+                            'name': 'GCRED',
+                            'value': gcred
+                        },
+                    ],
+                    'command': ['bash','-c','git clone https://github.com/DOH-JDJ0303/waphl-data.git && bash waphl-data/waphl-terra2res/aws-batch-script.sh $TERRA_PROJECT $TERRA_WORKSPACE $TERRA_SUBMISSIONID $TERRA_WORKFLOW $TERRA_SUBMISSIONENTITY $S3_OUTDIR $GCRED']
+                }
+            )
 
 def handler(event, context):
     # get secrets
-    secret_name = "terraRunChecker/1"
+    secret_name = "terraRunChecker/2"
     region_name = "us-west-2"
 
     # Create a Secrets Manager client
@@ -74,22 +108,22 @@ def handler(event, context):
     terra_project      = secret["terra_project"]
     terra_workspaces   = secret["terra_workspaces"].split(',')
     aws_results_bucket = secret["aws_results_bucket"]
-    aws_prod_sqs_url   = secret["aws_prod_sqs_url"]
+    aws_job_queue      = secret["aws_job_queue"]
+    aws_job_def        = secret["aws_job_def"]
     google_credentials = secret["google_credentials"]
 
     # set gcloud credentials
-    google_credentials = "s3://waphl-results/assets/.config/gcloud/application_default_credentials.json"
     s3_client    = boto3.client('s3')
     gcred_split  = google_credentials.replace('s3://','').split('/')
     gcred_bucket = gcred_split[0]
     gcred_key    = '/'.join(gcred_split[1:])
-    gcred_local  = "google_credentials.json"
+    gcred_local  = "/tmp/google_credentials.json"
     s3_client.download_file(gcred_bucket, gcred_key, gcred_local)
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcred_local
 
     # iterate over workspaces
     for wksp in terra_workspaces:
-        terraRunChecker(terra_project, wksp, aws_results_bucket, aws_prod_sqs_url)
+        terraRunChecker(terra_project, wksp, aws_results_bucket, aws_job_queue, aws_job_def, google_credentials)
 
 # # for dev
 # handler('test','test')
